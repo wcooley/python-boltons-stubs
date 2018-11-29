bcdb8581cc79bdfcdb97e50b5de5903a66b05222
Wil Cooley <wcooley@nakedape.cc>
2018-11-28 16:53:37 -0800

    Add debugutils

---
+++ boltons-stubs/debugutils.pyi
@@ -0,0 +1,17 @@
+from typing import (
+    Any,
+    Dict,
+    Tuple,
+    Union,
+)
+
+
+def trace_print_hook(
+    event: str,
+    label: str,
+    obj: Union[Dict[str, str], bytes],
+    attr_name: str,
+    args: Tuple = (),
+    kwargs: Dict[Any, Any] = {},
+    result: Any = ...,
+) -> None: ...
